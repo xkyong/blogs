48d74ad6e240c4bacd70256ed95a0f1eaac152f7
xkyong <xky13802315708@163.com>
2023-07-27 21:48:57 +0800

    feat: 新增访问人数显示；使用medium-zoom使文章图片支持zoomin和zoomout

---
+++ docs/.vitepress/theme/components/Analysis.vue
@@ -0,0 +1,16 @@
+<template>
+  <span style="font-size: 0.75rem; line-height: 1rem; padding: 0 0.5rem;">今日访问人数 {{ count }}</span>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const setVisitUrl = 'http://8.129.234.28:9090/blog/setVisit'
+
+const count = ref(0)
+
+;(async () => {
+  const { data } = await axios.get(setVisitUrl)
+  count.value = data.data
+})()
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,37 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import { h, onMounted, nextTick, watch } from 'vue'
+import { useRoute } from 'vitepress'
 import Theme from 'vitepress/theme'
+import mediumZoom from 'medium-zoom'
+
 import './style.css'
+
+import Analysis from './components/Analysis.vue'
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'nav-bar-content-before': () => h(Analysis)
     })
   },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup () {
+    const route = useRoute()
+
+    const initZoom = () => {
+      // 文章所有图片均支持 zoomin 和 zoomout
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    }
+
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+
+    onMounted(() => initZoom())
   }
 }
