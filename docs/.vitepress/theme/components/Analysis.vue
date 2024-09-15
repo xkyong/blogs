7c98fba5a178a38bfd0b1e88e5bcfc1f2bb4847e
xkyong <xky13802315708@163.com>
2024-09-15 11:16:44 +0800

    style: 修改样式

--- docs/.vitepress/theme/components/Analysis.vue
+++ docs/.vitepress/theme/components/Analysis.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="display: flex; gap: 10px;">
+  <div style="margin-left: 10px">
     <a href="https://xkyong.github.io/blogs/">
       <img src="https://badges.pufler.dev/visits/xkyong/blog" alt="visit badge" data-canonical-src="https://xkyong.github.io/" style="max-width: 100%;">
     </a>
