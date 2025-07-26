f921fb80753fcba363fda073281010cd5d9e8d21
xkyong <xky13802315708@163.com>
2025-07-26 17:59:52 +0800

    chore: 修改访问量获取链接

--- docs/.vitepress/theme/components/Analysis.vue
+++ docs/.vitepress/theme/components/Analysis.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="margin-left: 10px">
     <a href="https://xkyong.github.io/blogs/">
-      <img src="https://badges.pufler.dev/visits/xkyong/blog" alt="visit badge" data-canonical-src="https://xkyong.github.io/" style="max-width: 100%;">
+      <img src="https://visitor-badge.laobi.icu/badge?page_id=xkyong.blog" alt="visit badge" data-canonical-src="https://xkyong.github.io/" style="max-width: 100%;">
     </a>
   </div>
 </template>
